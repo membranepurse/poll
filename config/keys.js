d8f08501f85d986b68fafcdcbc0d7a39d2586d4e
solvedsager <sager.brown@gmail.com>
2019-04-14 21:33:14 -0500

    init

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: 'mongodb://sbrown:vikings1@ds117145.mlab.com:17145/heroku_26nqc5w5',
+  pusherAppId: '742511',
+  pusherKey: '890b8897c8c0b0daede0',
+  pusherSecret: '56d48541a2554e7407e5',
+  pusherCluster: 'us2',
+  pusherEncrypted: true
+};
